6820f73d59fa1df4917ed2d6f1dd632e07341b9c
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-05-24 11:15:32 +0530

    Initial commit👍

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MainHeader} from './core/main-header/main-header';
+import {MainFooter} from './core/main-footer/main-footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainHeader, MainFooter],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
