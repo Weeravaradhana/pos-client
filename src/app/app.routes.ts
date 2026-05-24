45d08cb401313c2286600f429588b8183a510bba
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-05-24 13:25:31 +0530

    chore(router): consolidate route configuration and remove empty duplicate routes file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import {HomePageContext} from './pages/home-page-context/home-page-context';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"", redirectTo: "/home/process", pathMatch: "full" },
+  {path:"home",
+    component:HomePageContext,
+    children:[
+      {path:"", redirectTo: "/home/process", pathMatch: "full" },
+
+      {path:"process",
+        loadComponent:() =>
+          import('./pages/home-page/home-page').then(ob=>ob.HomePage),
+        title:"POS | Home"
+      },
+
+      {path:"login",
+        loadComponent:() =>
+          import('./pages/auth/login/login').then(ob=>ob.Login),
+        title:"POS | Login"
+      },
+
+      {path:"signup",
+        loadComponent:() =>
+          import('./pages/auth/register/register').then(ob=>ob.Register),
+        title:"POS | Signup"
+      },
+
+    ]},
+
+  {path:"dashboard", loadComponent:() =>
+      import('./pages/dashboard-page/dashboard-page').then(ob=>ob.DashboardPage),
+    title:"POS | Dashboard"
+  },
+
+  {path:"**",loadComponent:()=>
+  import('./pages/not-found/not-found').then((ob)=>ob.NotFound),
+    title:"404 | Not Found"
+  },
+];
