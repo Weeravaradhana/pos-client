ee7d94093c509ee4b00be6c4b9ca00612cfdb3e1
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-05-26 12:46:08 +0530

    feat(customer): implement update dialog with injected data and prefilled reactive form

---
+++ src/app/pages/dashboard-page/inner-pages/customers/inner/customer-update-dialog-component/customer-update-dialog-component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CustomerUpdateDialogComponent } from './customer-update-dialog-component';
+
+describe('CustomerUpdateDialogComponent', () => {
+  let component: CustomerUpdateDialogComponent;
+  let fixture: ComponentFixture<CustomerUpdateDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CustomerUpdateDialogComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CustomerUpdateDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/dashboard-page/inner-pages/customers/inner/customer-update-dialog-component/customer-update-dialog-component.ts
@@ -0,0 +1,52 @@
+import {Component, Inject} from '@angular/core';
+import {MatIcon} from '@angular/material/icon';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {NgIf} from '@angular/common';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {Customer} from '../../../../../../dto/Customer';
+
+@Component({
+  selector: 'app-customer-update-dialog-component',
+  imports: [
+    MatIcon,
+    ReactiveFormsModule,
+    NgIf
+  ],
+  templateUrl: './customer-update-dialog-component.html',
+  styleUrl: './customer-update-dialog-component.scss',
+})
+export class CustomerUpdateDialogComponent {
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<CustomerUpdateDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Customer
+  ) {
+    this.form = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      address: ['', [Validators.required]],
+      salary: [null, [Validators.required, Validators.min(0)]],
+    });
+  }
+
+  ngOnInit(): void {
+    this.form.patchValue({
+      name: this.data.name,
+      address: this.data.address,
+      salary: this.data.salary,
+    });
+  }
+
+  save(): void {
+    if (this.form.valid) {
+      this.dialogRef.close({ ...this.data, ...this.form.value });
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+
+  close(): void {
+    this.dialogRef.close();
+  }
+}
