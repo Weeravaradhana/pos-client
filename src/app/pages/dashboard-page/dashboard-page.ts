57d47e678d7fb9a6e4e451c7a6a2a55b04c36e28
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-05-26 12:39:53 +0530

    feat(dashboard): implement dashboard page with tab navigation and nested route support

--- src/app/pages/dashboard-page/dashboard-page.ts
+++ src/app/pages/dashboard-page/dashboard-page.ts
@@ -1,10 +1,28 @@
 import {Component} from '@angular/core';
+import {DashboardMainHeader} from './inner-pages/core/dashboard-main-header/dashboard-main-header';
+import {DashboardMainFooter} from './inner-pages/core/dashboard-main-footer/dashboard-main-footer';
+import {TabBar} from '../../dto/TabBar';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports:[],
+  imports: [
+    DashboardMainHeader,
+    DashboardMainFooter,
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet
+  ],
   templateUrl:'./dashboard-page.html',
   styleUrls: ['./dashboard-page.scss'],
 
 })
-export class DashboardPage {}
+export class DashboardPage {
+  tabs: TabBar[] = [
+    { label: 'Customers', path: 'customers', icon: '👥' },
+    { label: 'Products', path: 'products', icon: '📦' },
+    { label: 'Orders', path: 'orders', icon: '🧾' },
+    { label: 'Place Order', path: 'place-order', icon: '➕' },
+    { label: 'Statistics', path: 'statistics', icon: '📊' },
+  ];
+}
