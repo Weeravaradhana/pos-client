23e158117623140b4ad3cec6d288aa870ebbda96
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-05-26 12:38:22 +0530

    feat(router): add protected dashboard routes with auth guard and lazy-loaded management pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import {HomePageContext} from './pages/home-page-context/home-page-context';
+import {authGuard} from './guards/auth-guard';
 
 export const routes: Routes = [
   {path:"", redirectTo: "/home/process", pathMatch: "full" },
@@ -28,9 +29,55 @@ export const routes: Routes = [
 
     ]},
 
-  {path:"dashboard", loadComponent:() =>
-      import('./pages/dashboard-page/dashboard-page').then(ob=>ob.DashboardPage),
-    title:"POS | Dashboard"
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./pages/dashboard-page/dashboard-page').then((ob) => ob.DashboardPage),
+    title: 'Dashboard',
+    canActivate: [authGuard],
+    children: [
+      {
+        path: '',
+        redirectTo: '/dashboard/customers',
+        pathMatch: 'full',
+      },
+      {
+        path: 'customers',
+        loadComponent: () =>
+          import('./pages/dashboard-page/inner-pages/customers/customers').then(
+            (ob) => ob.Customers,
+          ),
+        title: 'Customer MGT',
+      },
+      {
+        path: 'products',
+        loadComponent: () =>
+          import('./pages/dashboard-page/inner-pages/products/products').then((ob) => ob.Products),
+        title: 'Product MGT',
+      },
+      {
+        path: 'orders',
+        loadComponent: () =>
+          import('./pages/dashboard-page/inner-pages/orders/orders').then((ob) => ob.Orders),
+        title: 'Order MGT',
+      },
+      {
+        path: 'place-order',
+        loadComponent: () =>
+          import('./pages/dashboard-page/inner-pages/place-order/place-order').then(
+            (ob) => ob.PlaceOrder,
+          ),
+        title: 'Place Order',
+      },
+      {
+        path: 'statistics',
+        loadComponent: () =>
+          import('./pages/dashboard-page/inner-pages/statistics/statistics').then(
+            (ob) => ob.Statistics,
+          ),
+        title: 'Reports & Statistics',
+      },
+    ],
   },
 
   {path:"**",loadComponent:()=>
